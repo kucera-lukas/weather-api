268491dc42aa43fe4876bffc4bfebeadae283489
kucera-lukas <lukas.kucera.g@protonmail.com>
2021-06-15 18:51:17 +0200

    lru cache dates

--- weather_api/apps/app_forecast/dates.py
+++ weather_api/apps/app_forecast/dates.py
@@ -1,7 +1,8 @@
 """Parse/format string/datetime objects."""
 from __future__ import annotations
 
-from datetime import datetime, timedelta
+import functools as fn
+from datetime import datetime
 from typing import TYPE_CHECKING
 
 
@@ -9,6 +10,7 @@ if TYPE_CHECKING:
     from datetime import date
 
 
+@fn.cache
 def date_to_api_format(dt: date, /) -> str:
     """Return a date in ``WeatherAPI`` datetime format.
 
@@ -18,6 +20,7 @@ def date_to_api_format(dt: date, /) -> str:
     return str(dt.strftime("%Y-%m-%d"))
 
 
+@fn.cache
 def api_str_to_date(dt: str, /) -> date:
     """Return string converted from api request to a ``date`` object.
 
@@ -34,5 +37,16 @@ def validate_date(dt: date, /) -> bool:
     :param dt: Date to validate
 
     """
-    now = datetime.now()
+    now = datetime.today()
     return dt > now
+
+
+def days_in_advance(dt: date, /) -> int:
+    """Return how many days are there between now and the given date.
+
+    :param dt: The date to check
+
+    """
+    now = datetime.today()
+    delta = dt - now
+    return delta.days
