a53de92c151cf98a3cb1b2cc37365b4de9812fd4
kucera-lukas <lukas.kucera.g@protonmail.com>
2021-06-16 17:29:30 +0200

    add test_exceptions

---
+++ weather_api/apps/app_forecast/tests/test_exceptions.py
@@ -0,0 +1,28 @@
+"""Test the app exceptions module."""
+from __future__ import annotations
+
+import json
+
+import pytest
+from weather_api.apps.app_forecast.exceptions import InvalidQueryParams
+
+
+@pytest.mark.parametrize(
+    "status_code, message, field, value",
+    [(200, "exception test message", "field", "value")],
+)
+def test_invalid_query_params_repr(
+    status_code: int,
+    message: str,
+    field: str,
+    value: str,
+) -> None:
+    """Test the __repr__ method of ``InvalidQueryParams``."""
+    obj = InvalidQueryParams(
+        status_code=status_code,
+        message=message,
+        field=field,
+        value=value,
+    )
+
+    assert json.loads(repr(obj)) == {"detail": message, "field": field, "value": value}
